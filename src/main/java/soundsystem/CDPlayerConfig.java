4d5094466980ab9ba30ca039a53df36c9f5e5e47
조현우 <2008517@clt.net>
2018-04-06 11:54:46 +0900

    Chapter 2 finished

--- src/main/java/soundsystem/CDPlayerConfig.java
+++ src/main/java/soundsystem/CDPlayerConfig.java
@@ -1,10 +1,14 @@
 package soundsystem;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages= {"soundsystem", "video"})
-public class CDPlayerConfig {
 
+public class CDPlayerConfig {
+	
+	@Bean
+	public CDPlayer cdPlayer(CompactDisc compactDisc) {
+		return new CDPlayer(compactDisc);
+	}
 }
--- src/test/java/soundsystem/CDPlayerTest.java
+++ src/test/java/soundsystem/CDPlayerTest.java
@@ -1,16 +1,26 @@
 package soundsystem;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=CDPlayerConfig.class)
+@ContextConfiguration(classes=SoundSystemConfig.class)
 public class CDPlayerTest {
+	@SuppressWarnings("deprecation")
+	@Rule
+	public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+	
+	@Autowired
+	private MediaPlayer player;
+	
 	@Autowired
 	private CompactDisc cd;
 	
@@ -18,5 +28,12 @@ public class CDPlayerTest {
 	public void cdShouldNotBeNull() {
 		assertNotNull(cd);
 	}
-
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void play() {
+		player.play();
+		assertEquals(
+				"Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\r\n", log.getLog());
+	}
 }
